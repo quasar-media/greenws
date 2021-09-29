6b4636d1cad24642a6fbdc6f82902bc132b43be9
Auri <me@aurieh.me>
2021-09-29 21:59:31 +0300

    Publish 0.1.0a3

--- src/greenws.py
+++ src/greenws.py
@@ -22,7 +22,7 @@ import wsproto.events
 import wsproto.utilities
 from gevent.timeout import Timeout  # re-export
 
-__version__ = "0.1.0a2"
+__version__ = "0.1.0a3"
 
 #: A proposed connection.
 #:
